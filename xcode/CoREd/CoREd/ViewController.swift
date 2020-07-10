875ce0520a0223a4b3c9e824b45e4bf8b8fd84a0
Klaus Wenger <kjwenger@yahoo.com>
2020-07-10 16:49:11 +0200

    initial load

---
+++ xcode/CoREd/CoREd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoREd
+//
+//  Created by Klaus Wenger on 10.07.20.
+//  Copyright © 2020 Klaus Wenger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
